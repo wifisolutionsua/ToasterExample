fac8f7ccbb4fad7a01ee374f359bb2253d101dca
serg <sergimac@inwhite.com.ua>
2023-10-03 10:41:13 +0300

    Added second function

--- ToasterLibrary/src/main/java/com/wifisolutions/toasterlibrary/ToasterMessage.java
+++ ToasterLibrary/src/main/java/com/wifisolutions/toasterlibrary/ToasterMessage.java
@@ -10,4 +10,10 @@ public class ToasterMessage {
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
     }
+
+    public static void showTheToast(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
 }
